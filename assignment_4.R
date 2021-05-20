ba75d49c97eb90cccd7467421aaa69798f08ac7b
Ian Brunjes <ianbrunjes@ucsb.edu>
2021-05-20 15:02:45 -0700

    Updated script and added R markdown + knitted document for answers.

--- assignment_4.R
+++ assignment_4.R
@@ -21,12 +21,12 @@ pulse <- 35*10^9 # tons of CO2
 
 # Fit quadratic model
 data_dmg <- data_dmg %>% mutate(warming_sq = warming^2)
-quad_dmg <- lm(damages ~ warming + warming_sq + 0, data = data_dmg)
+quad_dmg <- lm(damages ~ warming_sq + warming + 0, data = data_dmg)
 #summary(quad_dmg)
 
 # Store output into funtion
-damages_model <- function(warming) {
-  out <- quad_dmg$coefficients[["warming_sq"]]*(warming^2) + quad_dmg$coefficients[["warming"]]*warming
+damages_model <- function(x) {
+  out <- quad_dmg$coefficients[["warming_sq"]]*(x^2) + quad_dmg$coefficients[["warming"]]*x + 0
   return(out)
 }
 
@@ -105,49 +105,4 @@ ggplot(data = scc_points, mapping = aes(x = discount_rate * 100, y = scc)) +
   labs(x = "Discount Rate (%)", y = "SCC ($)")
 
 
-####
-# 4.
-####
 
-# Ramsey Rule variables
-p <- 0.001
-n <- 2
-g <- 0.01
-
-ramsey_rule <- function(p, n, g) {
-  p + n*g
-}
-
-rr_discount <- ramsey_rule(p,n,g)
-rr_scc <- PV_calc(data_wrm$dmg_per_ton, rr_discount)
-
-# This will depend on #3
-# scc_rr <- scc_model(rr_discount_pct)
-ggplot(data = scc_points, mapping = aes(x = discount_rate * 100, y = scc)) +
-  geom_line() +
-  labs(x = "Discount Rate (%)", y = "SCC ($)") +
-  geom_point(aes(x = rr_discount_pct, y = rr_scc), col = "red", size = 3)
-
-
-####
-# 5.
-####
-
-# Scenario A
-data_wrm <- data_wrm %>%
-  mutate(warming_baseline_150 = warming_baseline * 1.5) %>%
-  mutate(predicted_baseline_150 = damages_model(warming_baseline_150)) %>%
-  mutate(warming_baseline_2050 = 
-           case_when( year > 2050 ~ 1.29,
-                     TRUE ~ warming_baseline
-          )) %>% 
-  mutate(predicted_baseline_2050 = damages_model(warming_baseline_2050))
-  
-
-A_baseline <- PV_calc(data_wrm$predicted_baseline, 0.02)
-A_baseline_150 <- PV_calc(data_wrm$warming_baseline_150, 0.02)
-
-A_expected_value <- (A_baseline * 0.5) + (A_baseline_150 * 0.5)
-B_expected_value <- PV_calc(data_wrm$predicted_baseline_2050, 0.02)
-
-# How to get "max cost"
